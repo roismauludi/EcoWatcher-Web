aeee20879eb9251c9b813b62b643b7dd5452f317
roismauludi <roismauludi73.com>
2025-06-27 23:23:00 +0700

    update tailwind mobile3

--- example/components/Header.tsx
+++ example/components/Header.tsx
@@ -30,29 +30,10 @@ const Header: React.FC = () => {
             />
           </svg>
         </button>
-        <div className="flex justify-center flex-1 lg:mr-32">
-          <div className="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
-            <div className="absolute inset-y-0 flex items-center pl-2">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <input
-              className="w-full pl-8 pr-4 text-sm text-gray-700 dark:text-gray-200 border rounded-lg dark:bg-gray-700 dark:border-gray-600 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
-              type="text"
-              placeholder="Search for projects"
-              aria-label="Search"
-            />
-          </div>
+        <div className="flex-1 flex justify-center">
+          <span className="text-2xl font-extrabold text-purple-600 dark:text-purple-200 tracking-wide">
+            EcoWatcher
+          </span>
         </div>
         <ul className="flex items-center flex-shrink-0 space-x-6">
           <li className="flex">
@@ -66,20 +47,6 @@ const Header: React.FC = () => {
               ) : (
                 <MoonIcon className="w-5 h-5" />
               )}
-            </button>
-          </li>
-          <li className="relative">
-            <button
-              className="relative align-middle rounded-md focus:outline-none focus:shadow-outline-purple"
-              aria-label="Account"
-              aria-haspopup="true"
-            >
-              <img
-                className="object-cover w-8 h-8 rounded"
-                src="https://images.unsplash.com/photo-1502378735452-bc7d3b7a0a5f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707c9c7fc429d10cdd1aebccdb62a041"
-                alt=""
-                aria-hidden="true"
-              />
             </button>
           </li>
         </ul>
